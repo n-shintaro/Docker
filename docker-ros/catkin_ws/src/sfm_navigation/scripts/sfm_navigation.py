94a0dd3c26beb30dab96dc4c37b415d04d715225
n-shintaro <nakaoka.shintaro.infratop@gmail.com>
2019-09-15 14:38:16 +0900

    wall visiuations

--- docker-ros/catkin_ws/src/sfm_navigation/scripts/sfm_navigation.py
+++ docker-ros/catkin_ws/src/sfm_navigation/scripts/sfm_navigation.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from agent import *
 from config import *
+from visualization_msgs.msg import MarkerArray,Marker
 from geometry_msgs.msg import PoseArray, Pose
 #import pdb; pdb.set_trace()
 # global変数
@@ -34,7 +35,7 @@ class SFMNavigation:
 
 
     # 速度指令 Publisher ----------------------------------------------------
-        self.position_publisher = rospy.Publisher('cml_pos', PoseArray, queue_size=10)
+        self.marker_publisher = rospy.Publisher('visualization_marker', MarkerArray, queue_size=10)
     #===========================================================================
     #   Calculate SFM
     #===========================================================================
@@ -42,7 +43,26 @@ class SFMNavigation:
     def social_force_model(self,agents, walls):
         # 相互作用を計算
         while not rospy.is_shutdown():
-            posearray = PoseArray()
+            marker_array = MarkerArray()
+            for idxi,wi in enumerate(walls):
+                marker_wall = Marker()
+                marker_wall.header.frame_id = "world"
+                marker_wall.header.stamp = rospy.Time.now()
+                marker_wall.pose.position.x = walls[idxi][0]
+                marker_wall.pose.position.y = walls[idxi][1]
+                marker_wall.type = 2
+                marker_wall.ns = "wall"
+                marker_wall.action=marker_wall.ADD
+                marker_wall.id = idxi
+                marker_wall.color.r = 0.0
+                marker_wall.color.g = 0.0
+                marker_wall.color.b = 1.0
+                marker_wall.color.a = 1.0
+
+                marker_wall.scale.x = 0.5
+                marker_wall.scale.y = 0.5
+                marker_wall.scale.z = 0.5
+                marker_array.markers.append(marker_wall)
             r = rospy.Rate(40)     # 20Hz
             for idxi,ai in enumerate(agents):# idxi: インデックス　ai：要素
 
@@ -76,13 +96,26 @@ class SFMNavigation:
                 # 位置をpublish
                 # 位置を指定
                 #import pdb; pdb.set_trace()
-                pose = Pose()
-                pose.position.x = ai.pos[0]
-                pose.position.y=ai.pos[1]
-                posearray.poses.append(pose)
+                marker_agent = Marker()
+                marker_agent.header.frame_id = "world"
+                marker_agent.header.stamp = rospy.Time.now()
+                marker_agent.pose.position.x = ai.pos[0]
+                marker_agent.pose.position.y = ai.pos[1]
+                marker_agent.type = 2
+                marker_agent.ns = "agent"
+                marker_agent.action=marker_agent.ADD
+                marker_agent.id = idxi
+                marker_agent.color.r = 1.0
+                marker_agent.color.g = 0.0
+                marker_agent.color.b = 0.0
+                marker_agent.color.a = 1.0
+
+                marker_agent.scale.x = 0.5
+                marker_agent.scale.y = 0.5
+                marker_agent.scale.z = 0.5
+                marker_array.markers.append(marker_agent)
                 # フレームを指定
-                posearray.header.frame_id = "world"
-            self.position_publisher.publish(posearray)
+            self.marker_publisher.publish(marker_array)
             r.sleep()
 
 ################################################################################
